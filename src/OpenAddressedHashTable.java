20ab3594de6486c68b5a3d12aaa819b8c644eba2
Andrew P. <apetricc@unca.edu>
2015-10-19 12:09:04 -0400

    Update OpenAddressedHashTable.java
    final version I turned in by email

--- src/OpenAddressedHashTable.java
+++ src/OpenAddressedHashTable.java
@@ -1,25 +1,116 @@
 /**
- * Created by aj on 10/10/15.
+ * Andrew Petriccione csci 333 homework 6--hash tables
  */
 
 /**
- * 24 points: After your ChainedHashTable class is working, create another separate Java class called
- * OpenAddressedHashTable in the same package, based on a hash table with open addressing using linear probing
- * from the slides/textbook. Your should use an array of Integer objects as your primary instance variable / data
- * field. The auxiliary hash function should use the multiplication method or can come from a universal
- * collection -- either way, you can reuse your hash function from the ChainedHashTable class.
- The constructor should have a parameter for n, the total number of inputs expected. In the constructor,
- create the array of size m as an array of Integer of the same array length as the ChainedHashTable class's array.
- There should also be public methods for insert, delete, and search as in the slides/textbook. To facilitate your
- delete method, you may want to define a static final int instance variable / class data field named DELETED,
- whose value is Integer.MIN_VALUE.
- I also want a public printTable method, which runs a loop and prints each Integer in the hash table.
- With Java's handy auto-unboxing feature, this will be a very straight-forward printing loop.
- You will need a private helper method with one parameter named hash, to implement the auxiliary hash function.
- You will need another private helper method with two parameters named hash, to implement the primary hash
- function. Given a key and an "index" into the probe sequence, it returns the appropriate element of the probe
- sequence, which is an index into the hash table's main array. It will do this by invoking the
- one-parameter hash method.
+ * The openAddressedHashTable class makes a hash table using the open
+ * addressed hash table algorithm
  */
-public class OpenAddressedHashTable {
+public class openAddressedHashTable {
+    private int m;
+    private Integer[] theList;
+    private static int DELETED = Integer.MIN_VALUE;
+    private static double A = 0.618;
+
+    /**
+     * The openAddressedHashTable constructor method creates an open addressed
+     * hash table using the algorithm for open addressed hash tables
+     * @param n The total number of input elements in the open addressed hash table.
+     */
+    public openAddressedHashTable(int n) {
+        m = 1;
+        while (m <= n) {
+            m *= 2;
+        }
+        theList = new Integer[m];
+    }
+
+    /**
+     * The insert method inserts a value into the hash table
+     * @param n the value to be inserted
+     * @return the index where the key was placed in the hash table, or else NULL
+     * if the table is full
+     */
+    public Integer insert(Integer n) {
+        int k = n.intValue();
+        for (int i = 0; i < m; i++) {
+            Integer j = hash(k, i);
+            if (theList[j] == null) {
+                theList[j] = n;
+                return j;
+            }
+        }
+        System.out.println("Hash Table Overflow");
+        return null;
+    }
+
+    /**
+     *The delete method removes a key from the hash table.
+     * @param n the key to be removed from the hash table
+     * @return the index where the deleted element was in the hash table,
+     * or else NULL if the key wasn't found
+     */
+    public Integer delete(Integer n) {
+        int k = n.intValue();
+        for (int i = 0; i < m; i++) {
+            Integer j = hash(k, i);
+            if (theList[j] == k) {
+                theList[j] = DELETED;
+                return j;
+            }
+        }
+        System.out.println("Element not found");
+        return null;
+    }
+
+    /**
+     *  The search method looks in the hash table for the key given
+     * @param k the key to search for
+     * @return the index where the key is if it's found,
+     * else return NULL if key not found
+     */
+    public Integer search(int k) {
+        for (int i = 0; i < m; i++) {
+            Integer j = (int) hash(k, i);
+            //if (theList[j] == DELETED) return null ?;
+            if (theList[j] == null) return null;
+            if (theList[j] == k) return theList[j];
+        }
+        return null;
+    }
+
+    /**
+     * The printTable method converts the table into a String that can be printed
+     * @return The String to be printed
+     */
+    public String printTable() {
+        String result = "";
+        for (int i = 0; i < theList.length; i++) {
+            if (theList[i] != null)
+                result += "[" + theList[i].toString() + "]";
+            else result += "[]";
+        }
+        return result;
+    }
+
+    /**
+     * the auxiliary hash method is a helper method for the main hash method
+     * @param k the key to insert into the hash table
+     * @return the index where the key k will be placed in the hash table
+     */
+    private int hash(int k) {
+        double hashTag = (m * (k * A % 1));
+        return (int) hashTag;
+    }
+
+    /**
+     * This hash function uses probing to place keys into the hash table.
+     * @param k The key to be inserted into the hash table
+     * @param i the distance to the next location on the hash table.
+     * @return the index of where to place key k in the hash table.
+     */
+    private int hash(int k, int i) {
+        int hashTag = (hash(k) + i) % m;
+        return hashTag;
+    }
 }
